8f47c8a681a7907ff8f163c43925baae753e42ab
lihachthebest@gmail.com <lihachthebest@gmail.com>
2018-03-29 15:00:34 +0300

    adding pattern, value checker, table

--- src/main/java/sample/view/MainController.java
+++ src/main/java/sample/view/MainController.java
@@ -1,16 +1,22 @@
 package sample.view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import sample.Objects.Channel;
 import sample.Objects.EmpiricalCoefficients;
 import sample.Objects.Material;
+import sample.Objects.ValueAdapter;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.regex.Pattern;
 
 public class MainController {
 
@@ -46,11 +52,52 @@ public class MainController {
     private NumberAxis yAxisCons = new NumberAxis();
     private NumberAxis xAxisCons = new NumberAxis();
 
+    public TableView<ValueAdapter> reportView;
+    public TableColumn<ValueAdapter, Integer> numberColumn;
+    public TableColumn<ValueAdapter, String> lenghtColumn;
+    public TableColumn<ValueAdapter, String> consistColumn;
+    public TableColumn<ValueAdapter, String> temperatureColumn;
+
+    private ObservableList<ValueAdapter> values = FXCollections.observableArrayList();
+
     public MainController() {
     }
 
     public void initialize() {
+        numberColumn.setCellValueFactory(new PropertyValueFactory<ValueAdapter, Integer>("number"));
+        lenghtColumn.setCellValueFactory(new PropertyValueFactory<ValueAdapter, String>("lenght"));
+        consistColumn.setCellValueFactory(new PropertyValueFactory<ValueAdapter, String>("consist"));
+        temperatureColumn.setCellValueFactory(new PropertyValueFactory<ValueAdapter, String>("temperature"));
+        reportView.setItems(values);
 
+        Pattern p = Pattern.compile("(\\d+\\.?\\d*)?");
+        stepField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(!p.matcher(newValue).matches()) stepField.setText(oldValue);
+        });
+        temperatureField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(!p.matcher(newValue).matches()) temperatureField.setText(oldValue);
+        });
+        speedField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(!p.matcher(newValue).matches()) speedField.setText(oldValue);
+        });
+        lenghtField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(!p.matcher(newValue).matches()) lenghtField.setText(oldValue);
+        });
+        widthField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(!p.matcher(newValue).matches()) widthField.setText(oldValue);
+        });
+        heightField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(!p.matcher(newValue).matches()) heightField.setText(oldValue);
+        });
+        densityField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(!p.matcher(newValue).matches()) densityField.setText(oldValue);
+        });
+        heatField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(!p.matcher(newValue).matches()) heatField.setText(oldValue);
+        });
+        meltingField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(!p.matcher(newValue).matches()) meltingField.setText(oldValue);
+        });
     }
 
 
@@ -69,32 +116,36 @@ public class MainController {
         double meltingTemperature = 0;
 
         try {
-            temperature = Double.parseDouble(temperatureField.getText().replace(',','.'));
-            speed = Double.parseDouble(speedField.getText().replace(',','.'));
-            step = Double.parseDouble(stepField.getText().replace(',','.'));
+            temperature = Double.parseDouble(temperatureField.getText().replace(',', '.'));
+            speed = Double.parseDouble(speedField.getText().replace(',', '.'));
+            step = Double.parseDouble(stepField.getText().replace(',', '.'));
 
-            width = Double.parseDouble(widthField.getText().replace(',','.'));
-            height = Double.parseDouble(heightField.getText().replace(',','.'));
-            lenght = Double.parseDouble(lenghtField.getText().replace(',','.'));
+            width = Double.parseDouble(widthField.getText().replace(',', '.'));
+            height = Double.parseDouble(heightField.getText().replace(',', '.'));
+            lenght = Double.parseDouble(lenghtField.getText().replace(',', '.'));
 
-            density = Double.parseDouble(densityField.getText().replace(',','.'));
-            heat = Double.parseDouble(heatField.getText().replace(',','.'));
-            meltingTemperature = Double.parseDouble(meltingField.getText().replace(',','.'));
+            density = Double.parseDouble(densityField.getText().replace(',', '.'));
+            heat = Double.parseDouble(heatField.getText().replace(',', '.'));
+            meltingTemperature = Double.parseDouble(meltingField.getText().replace(',', '.'));
 
             System.out.println(density + " " + heat + " " + meltingTemperature);
 
             Channel channel = new Channel(width, height, lenght);
             Material material = new Material(density, heat, meltingTemperature);
 
-            if (step > 0 || speed > 0 || temperature > -273) {
+            if (step > 0 && speed > 0 && temperature > -273 && width > 0 && height > 0 && lenght > 0 && density > 0 && heat > 0 && meltingTemperature > 0 ) {
+                values.clear();
                 long startTime = System.currentTimeMillis();
 
                 LineChart<Number, Number> chart2dTemp = new LineChart<Number, Number>(xAxisTemp, yAxisTemp);
                 XYChart.Series seriesTemp = calculateTemperature(temperature, speed, step, channel, material, empCoef);
+                seriesTemp.setName("Зависимость температуры от длины канала");
                 chart2dTemp.getData().add(seriesTemp);
 
                 LineChart<Number, Number> chart2dConsist = new LineChart<Number, Number>(xAxisCons, yAxisCons);
-                XYChart.Series seriesConsistencies = calculateConsistencies(temperature, speed, step, channel, material, empCoef);
+                XYChart.Series seriesConsistencies = calculateConsistencies(temperature, speed, step, channel, material, empCoef, values);
+                //xAxisCons.setLabel("X");
+                seriesConsistencies.setName("Зависимость вязкость от длины канала");
                 chart2dConsist.getData().add(seriesConsistencies);
 
                 DependenceViscosity.getChildren().add(chart2dConsist);
@@ -103,7 +154,7 @@ public class MainController {
                 long timeSpent = System.currentTimeMillis() - startTime;
 
                 timerLabel.setText("Время выполнения: " + String.valueOf(timeSpent) + " мс");
-                performanceLabel.setText("Производительность: " + String.valueOf(performanceCalc(channel.getHeight(), channel.getWidth(), speed, material.getDensity())) + " Кг/Ч");
+                performanceLabel.setText("Производительность: " + String.valueOf(String.format("%.2f", performanceCalc(channel.getHeight(), channel.getWidth(), speed, material.getDensity()))) + " Кг/Ч");
 
             } else {
                 getAlert();
@@ -146,15 +197,20 @@ public class MainController {
         return series;
     }
 
-    private XYChart.Series calculateConsistencies(double temperature, double speed, double step, Channel channel, Material material, EmpiricalCoefficients empCoef) {
+    private XYChart.Series calculateConsistencies(double temperature, double speed, double step, Channel channel, Material material, EmpiricalCoefficients empCoef, ObservableList<ValueAdapter> values) {
 
         XYChart.Series series = new XYChart.Series();
+        NumberFormat formater = new DecimalFormat("#0");
+        NumberFormat formater1 = new DecimalFormat("#0.0");
 
         double Fch = 0.125 * Math.pow((channel.getHeight() / channel.getWidth()), 2) - 0.625 * (channel.getHeight() / channel.getWidth()) + 1;
         double Q = (channel.getWidth() * channel.getHeight() * speed) / 2 * Fch;
         double G = material.getDensity() * Q * 3600;// кг час
 
+        int a = 0;
+
         for (double i = 0; i <= channel.getLenght(); i = i + step) {
+            a++;
             double gamma = speed / channel.getHeight();
             double qGamma = channel.getWidth() * channel.getHeight() * empCoef.getConsistention() * Math.pow(gamma, empCoef.getIndexMaterial() + 1);
             double qAlpha = channel.getWidth() * empCoef.getHeatTransfer() * ((1 / empCoef.getViscosity()) - temperature + empCoef.getAlignmentTemperature());
@@ -166,6 +222,7 @@ public class MainController {
             double viscosityMaterial = consistention * Math.pow(gamma, empCoef.getIndexMaterial() - 1); //взякость для зависимости
 
             series.getData().add(new XYChart.Data(i, viscosityMaterial));
+            values.add(new ValueAdapter(a, formater1.format(i) , formater.format(viscosityMaterial), formater.format(T)));
         }
         return series;
     }
